3f2097350e7fe892868b4c4551e509c98518e684
Konstantin Lopuhin <kostia.lopuhin@gmail.com>
2018-02-08 10:36:20 +0300

    bump version after release

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ test_requirements = [
 
 setup(
     name='tensorboard_logger',
-    version='0.1.0',
+    version='0.1.0.dev',
     description='Log TensorBoard events without Tensorflow',
     long_description=readme + '\n\n' + history,
     author='Konstantin Lopuhin',
