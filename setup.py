21e758bafdc73596930b4f2102b0d3e5b8334194
Konstantin Lopuhin <kostia.lopuhin@gmail.com>
2018-02-08 10:28:55 +0300

    0.1.0 release

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ test_requirements = [
 
 setup(
     name='tensorboard_logger',
-    version='0.0.4',
+    version='0.1.0',
     description='Log TensorBoard events without Tensorflow',
     long_description=readme + '\n\n' + history,
     author='Konstantin Lopuhin',
